a43fee805eb0a19cfc7eb4e399634304ff1f36f4
noppefoxwolf <noppelabs@gmail.com>
2018-10-16 22:57:23 +0900

    attachment object is enum

--- GabKit/Classes/Extensions/IndifiniteObject.swift
+++ GabKit/Classes/Extensions/IndifiniteObject.swift
@@ -7,18 +7,22 @@
 
 import Foundation
 
-public class AttachmentObject: Decodable {
-  public let value: Any
+public enum AttachmentObject: Decodable {
+  case media(Attachment.Media)
+  case medias([Attachment.Media])
+  case url(Attachment.URL)
+  case unknown
   
-  required public init(from decoder: Decoder) throws {
+  public init(from decoder: Decoder) throws {
     let container = try decoder.singleValueContainer()
-    if let value = try? container.decode(Attachment.Media.self) {
-      self.value = value
-    } else if let value = try? container.decode([Attachment.Media].self) {
-      self.value = value
+    if let media = try? container.decode(Attachment.Media.self) {
+      self = .media(media)
+    } else if let medias = try? container.decode([Attachment.Media].self) {
+      self = .medias(medias)
+    } else if let url = try? container.decode(Attachment.URL.self) {
+      self = .url(url)
     } else {
-      let value = try container.decode(Attachment.URL.self)
-      self.value = value
+      self = .unknown
     }
   }
 }
